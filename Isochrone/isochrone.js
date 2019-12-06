386c7a79ddc254fa92144de158c95b1163cfb86a
Roman Timashev <roman@tmshv.ru>
2019-12-06 13:47:41 +0300

    + isochrone 1.0.0

---
+++ Isochrone/isochrone.js
@@ -0,0 +1,40 @@
+importScripts('latlng.js')
+
+setup(async () => ({
+    name: 'Isochrone',
+    version: '1.0.0',
+    description: 'Calculate isochrone'
+}))
+
+command('Isochrone', async () => {
+    const layerId = await requestState('app.currentLayerId')
+    const geometryType = await requestState(`layers.data.${layerId}.geometryType`)
+
+    if (geometryType !== 'Polygon') {
+        await notifyError('Cannot create isochrone on non Polygon layer')
+        return
+    }
+
+    const [lng, lat] = await requestPoint()
+    const answer = await requestInput([
+        ['intervals', 5],
+        ['radius', 1],
+        ['cellSize', 0.05],
+    ])
+    
+    try {
+        const res = await getJson('https://galton.urbica.co/api/foot/', {
+            intervals: answer.intervals,
+            radius: answer.radius,
+            cellSize: answer.cellSize,
+            lat,
+            lng,
+        })
+
+        await addFeatures(res)
+    } catch (error) {
+        console.error('Worker error', error)
+        await notifyError('Something went wrong. Failed to calculate isochrone')
+    }
+})
+
