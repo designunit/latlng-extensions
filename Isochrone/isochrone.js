2896da150fe42f5ce49d429f0917195c44b10683
Roman Timashev <roman@tmshv.ru>
2020-05-04 19:03:20 +0300

    isochrone fixed

--- Isochrone/isochrone.js
+++ Isochrone/isochrone.js
@@ -1,10 +1,8 @@
 importScripts('/latlng.js')
 
-console.log('NEW ISOCHRONE')
-
 setup(async () => ({
     name: 'Isochrone',
-    version: '1.0.2',
+    version: '1.0.3',
     description: 'Calculate isochrone'
 }))
 
@@ -19,15 +17,20 @@ command('Isochrone', async () => {
 
     const {lng, lat} = await requestPoint()
     const answer = await requestInput([
-        ['intervals', 5],
-        ['radius', 1],
-        ['cellSize', 0.05],
-        ['profile', 'foot'],
-    ])
+		['intervals', ['input', { label: 'Intervals', defaultValue: 5 }]],
+		['radius',    ['input', { label: 'Radius', defaultValue: 1 }]],
+		['cellSize',  ['input', { label: 'Cell Size', defaultValue: 0.05 }]],
+		['profile',   ['input', { label: 'Profile', defaultValue: 'foot' }]],
+        //['intervals', 5],
+        //['radius', 1],
+        //['cellSize', 0.05],
+        //['profile', 'foot'],
+    ], {
+		title: 'Input isochrone parameters',
+	})
     
     try {
         const url = `https://galton.urbica.co/api/${answer.profile}/`
-        //const url = 'http://localhost:4000'
 		console.log('WILL LOAD ISOCHRONE FROM', url)
         const res = await getJson(url, {
             intervals: answer.intervals,
