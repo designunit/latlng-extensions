91b233299b3f8a5be11213e0071499321e357ebe
Roman Timashev <roman@tmshv.ru>
2022-04-28 18:23:31 +0300

    add ulyanovsk

---
+++ Special/ulyanovks.js
@@ -0,0 +1,222 @@
+importScripts('https://unpkg.com/typograf@6.11.0/dist/typograf.js')
+importScripts('https://cdnjs.cloudflare.com/ajax/libs/markdown-it/10.0.0/markdown-it.min.js')
+importScripts('/latlng.js')
+
+const SOURCE_ID = '626ab0b5ed99e7000994b522'
+
+const typeLabel = new Map([
+	['idea', 'Идея'],
+	['problem', 'Проблема'],
+	['nice', 'Ценность'],
+])
+
+setup(async () => {
+	// const layerId = await requestLayer({
+	// 	geometryTypes: ['Point']
+	// })
+	return {
+	    name: 'Ulyanovsk',
+	    version: '1.0.0',
+	    description: 'survey',
+	    // options: {
+	    // 	layerId,
+	    // }
+	}
+})
+
+on('install', async event => {
+	overlay([
+        ['@', 'right-center', [
+            ['button', { icon: 'question', command: 'ShowHelp' }],
+        ]],
+    ])
+
+    showHelp()
+})
+
+on('idle', async event => {
+    await toolbar([
+        ['AddIdea', {
+        	label: 'Предложить идею',
+        	icon: 'bulb',
+        	color: '#FFD166',
+        }],
+        ['AddAttraction', {
+        	label: 'Описать ценность',
+        	icon: 'like',
+        	color: '#4DCCBD',
+        }],
+        ['AddProblem', {
+        	label: 'Описать проблему',
+        	icon: 'dislike',
+        	color: '#F25C63',
+        }],
+    ], {
+    	foldedLabel: 'Добавить',
+    })
+})
+
+on('feature.select', async event => {
+	const featureId = event.data.featureId
+	const layerId = event.data.layerId
+	if(!featureId){
+		return
+	}
+	
+    const fc = await requestFeatures([featureId])
+    
+    const feature = fc.features[0]
+    const geometryType = feature.geometry.type
+    assert(geometryType !== 'Point', new Error('Selected feature is not a point'))
+    
+	const type = feature.properties['type']
+    const title = typeLabel.get(type)
+    const comment = feature.properties['comment']
+
+    const md = new markdownit()
+	const raw = md.render([
+		`# ${title}`,
+		comment
+	].join('\n\n'));
+
+    const tp = new Typograf({locale: ['ru', 'en-US']})
+    const html = tp.execute(
+    	raw
+    )
+
+	await showMapPopup(feature.geometry.coordinates, ['html', { html, style: {
+		padding: 16,
+	}}])
+})
+
+command("AddIdea", async ctx => {
+	return AddFeature({
+		type: 'idea',
+		title: 'Идея',
+		placeholder: 'Опишите свою идею...',
+		label: 'Комментарий',
+	})
+})
+
+command("AddProblem", async ctx => {
+	return AddFeature({
+		type: 'problem',
+		title: 'Проблема',
+		placeholder: 'Опишите проблему...',
+		label: 'Комментарий',
+	})
+})
+
+command("AddAttraction", async ctx => {
+	return AddFeature({
+		type: 'nice',
+		title: 'Ценность',
+		placeholder: 'Расскажите свою историю...',
+		label: 'Комментарий',
+	})
+})
+
+command("ShowHelp", () => {
+	showHelp()
+})
+
+async function showHelp(){
+	const text = `
+# Ульяновско-Димитровградская агломерация
+
+Поделиться своим мнением просто:
+1. Выберите отметку идею, проблему или ценность
+2. Укажите место на карте, к которому относится ваш комментарий
+3. Выберите категорию вашего комментария
+4. Напишите ваш комментарий
+	`
+	const md = new markdownit()
+	const html = md.render(text)
+
+	await showPopup([
+		['html', { html }]
+	], {
+		title: 'Help',
+		submit: 'Got it',
+	})
+}
+
+async function AddFeature({type, title, placeholder, label}) {
+	const mobile = await requestState('layout.mobile')
+	const info = mobile
+		? 'Добавте точку на карте'
+		: 'Добавте точку на карте'
+	const info2 = mobile
+		? 'Наведите перекрестие и нажмите ОК'
+		: 'Кликните по карте'
+	const coord = await requestPoint(info2, info)
+	// const coord = await requestPoint('Кликни по карте', 'что-то произойдет')
+
+    const categories = [
+        'Жилой квартал',
+        'Досуг и отдых',
+        'Бизнес',
+        'Промышленность',
+        'Леса, парки',
+        'Спорт',
+        'Экология',
+        'Культурная ценность',
+        'Транспорт',
+        'Инфраструктура',
+        'Архитектура',
+        'Благоустройство',
+        'Общество',
+        'Другое',
+    ]
+    
+
+	const form = await requestInput([
+        ['category', ['select', { label: 'Категория' },
+            categories.map(value => ['option', { value }])
+        ]],
+        ['comment', ['text', {
+        	label,
+	        placeholder,
+	        required: 'Вы забыли оставить коментарий',
+        	rows: 12,
+        }]],
+        // ['email', ['input', {
+        // 	label: 'EMAIL',
+        // 	placeholder: 'Расскажите свою email...',
+        // 	// pattern: {
+        //  //        value: /^([A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4})?$/i,
+        //  //        message: "invalid email address"
+        //  //    }
+        // }]],
+    ], {
+    	title,
+    	submit: 'Добавить',
+    	cancel: 'Отмена',
+    })
+
+	const date = new Date()
+    const properties = {
+		comment: form.comment,
+		dateAdded: date.toString(),
+        type,
+    }
+
+	const f = {
+		type: 'FeatureCollection',
+		features: [
+			{
+	        	type: 'Feature',
+		        geometry: {
+		            type: 'Point',
+		            coordinates: [coord.lng, coord.lat]
+		        },
+		        properties,
+		    }
+	    ]
+	}
+
+    await addFeatures(f, {
+    	sourceId: SOURCE_ID,
+    })
+}
+
